7db9b3b2f66990b7507b313f6d8daa0b298ab1b0
André Ferreira <andre.s.ferreira.vip@gmail.com>
2022-04-13 21:49:54 +0100

    eliminate function completed (finally)

--- funcs.c
+++ funcs.c
@@ -15,18 +15,24 @@ void error_op_fl(){
 
 void back_main_menu(){
 
-    printf("\n\n\n\n\nA voltar para o menu principal\n\t[");
-    for(int temp_bmm = 0; temp_bmm < 10; temp_bmm++){
-        sleep(1);
+    //printf("\n\n\n\n\nA voltar para o menu principal\n\t[");
+
+    printf("\n\nPressione ENTER para voltar ao menu principal");
+    getch(); //Wait for ENTER key
+
+    printf("[");
+    for(int temp_bmm = 0; temp_bmm < 45; temp_bmm++){
+        usleep(50000); //Sleep for 50000 microseconds
         printf("#");
     }
 
-    sleep(1);
+    usleep(50000);
     printf("]");
-    sleep(2);
-    printf("\n\n\n\n");
-    main();
+    sleep(1);
+    system("cls||clear"); //Clear console (works in windows and linux)
+    main(); //Back to main menu
 }
+
 
 void introduzir(){
 
@@ -38,28 +44,62 @@ void introduzir(){
 }
 
 void eliminar(){
-    FILE *fp;
-    char str[MAXCHAR];
+    FILE *fp, *fp_temp;
+    char str[MAXCHAR], line_str[MAXCHAR];
+    char name[100], turma[2], info[10][100], file_name[] = {"C://Users//G512L//CLionProjects//ProjetoPPP//active_students.csv"}, temp_file_name[] = {"C://Users//G512L//CLionProjects//ProjetoPPP//temp.csv"};
+    //NAO ESQUECER DE USAR O MALLOC
+    int ano, num, ctrl;
 
-    fp = fopen("C://Users//G512L//CLionProjects//ProjetoPPP//active_students.csv","r");
 
-printf("%s", fp);
+
+    fp = fopen(file_name,"r");
+    fp_temp = fopen(temp_file_name, "w");
+
+    printf("%s", fp);
 
     char *token;
 
-    if(fp == NULL) {error_op_fl();}
+    if(fp == NULL) {error_op_fl();} //throw error if file is null
     else{
-        while( fgets(str, MAXCHAR, fp) != NULL){
-            token = strtok(str, ",");
-            while(token != NULL){
-                printf("%s ",  token);
-                token = strtok(NULL, ",");
+
+        printf("Introduza as informacoes do aluno que deseja eliminar do sistema: ");
+        printf("\nNome: ");
+        scanf("%s", &name);
+        printf("Ano: ");
+        scanf("%d", &ano);
+        printf("Turma: ");
+        scanf("%s", &turma);
+        printf("Numero de turma: ");
+        scanf("%d", &num);
+
+
+        while( fgets(str, MAXCHAR, fp) != NULL){ //do until it reaches the end of the file
+            strcpy(line_str, str); //copy current line to line_str
+            token = strtok(str, ","); //assign to token the words until the first ","
+
+            ctrl = 0;
+
+            while(token != NULL){//do until it reaches the end of the line
+                strcpy(info[ctrl], token);//copy the student's info to the info array
+                token = strtok(NULL, ","); //get the following words delimited by ","
+                ctrl++;
+
+            }
+            if((strcmp(info[0], name) == 0) && (atoi(info[2]) == ano) && (strcmp(info[3], turma) == 0) && (atoi(info[4]) == num)){} //if the student's info is correct then the student won't be copied to the new file, therefore it will be removed
+            else{
+                fprintf(fp_temp, "%s", line_str); //if the student's info is not correct then it's not the correct student to eliminate, so the student will be copied to the new file, therefore it remains untouched
             }
         }
     }
 
+    system("cls||clear"); //clear screen for asthetics
+    printf("\n\nAluno eliminado do sistema com sucesso!");
+
     fclose(fp);
-    back_main_menu();
+    fclose(fp_temp);
+    remove(file_name); //remove original file
+    rename(temp_file_name, file_name); //rename new file to the old file name
+    back_main_menu(); //go back to main menu
 }
 
 void listar(){
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 
 int main() {
     int n;
-    printf("_________________________________________________________________________\n|\t\t\tSISTEMA DO BAR\t\t\t\t\t|\n|\t\t\t\t\t\t\t\t\t|\n| 1- Introduzir dados de um novo aluno\t\t\t\t\t|\n| 2- Eliminar um aluno existente\t\t\t\t\t|\n| 3- Listar todos os alunos por ordem alfabetica\t\t\t|\n| 4- Listar os alunos com saldo abaixo de um determinado valor\t\t|\n| 5- Apresentar toda a informacao de um determinado aluno\t\t|\n| 6- Efetuar uma despesa por um determinado aluno\t\t\t|\n| 7- Carregar a conta de um aluno\t\t\t\t\t|\n_________________________________________________________________________\n--> ");
+    printf("|-----------------------------------------------------------------------|\n|\t\t\tSISTEMA DO BAR\t\t\t\t\t|\n|\t\t\t\t\t\t\t\t\t|\n| 1- Introduzir dados de um novo aluno\t\t\t\t\t|\n| 2- Eliminar um aluno existente\t\t\t\t\t|\n| 3- Listar todos os alunos por ordem alfabetica\t\t\t|\n| 4- Listar os alunos com saldo abaixo de um determinado valor\t\t|\n| 5- Apresentar toda a informacao de um determinado aluno\t\t|\n| 6- Efetuar uma despesa por um determinado aluno\t\t\t|\n| 7- Carregar a conta de um aluno\t\t\t\t\t|\n|-----------------------------------------------------------------------|\n\n--> ");
     scanf("%d", &n);
 
     switch (n) {
