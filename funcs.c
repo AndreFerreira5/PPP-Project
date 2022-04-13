d2cd0ba4dd957a516e1844f598d95da05d5b15e5
André Ferreira <andre.s.ferreira.vip@gmail.com>
2022-04-13 01:25:40 +0100

    working on funcs

--- funcs.c
+++ funcs.c
@@ -1,7 +1,32 @@
-//
-// Created by G512L on 4/5/2022.
-//
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h> //for Linux
+#include <Windows.h> //for Windows
 
+#define MAXCHAR 1000
+
+
+
+void error_op_fl(){
+    printf("Ocorreu um erro no acesso a base de dados!");
+    sleep(5);
+}
+
+void back_main_menu(){
+
+    printf("\n\n\n\n\nA voltar para o menu principal\n\t[");
+    for(int temp_bmm = 0; temp_bmm < 10; temp_bmm++){
+        sleep(1);
+        printf("#");
+    }
+
+    sleep(1);
+    printf("]");
+    sleep(2);
+    printf("\n\n\n\n");
+    main();
+}
 
 void introduzir(){
 
@@ -13,9 +38,20 @@ void introduzir(){
 }
 
 void eliminar(){
+    FILE *fp;
+    char str[MAXCHAR];
 
-    printf("eheheh");
+    fp = fopen("active_students.csv","r");
 
+    if(fp == NULL) {error_op_fl();}
+    else{
+        while( fgets(str, MAXCHAR, fp) != NULL){
+            printf("%s", str);
+        }
+    }
+
+    fclose(fp);
+    back_main_menu();
 }
 
 void listar(){
